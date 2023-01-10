da8a053f230d3d4bfa22b9f32521482c72d33345
Shahadat_Robin <robin@corexlab.dev>
2023-01-10 17:01:47 +0600

    redux with multiple reducers

--- index.js
+++ index.js
@@ -3,26 +3,74 @@ const redux = require("redux");
 const createStore = redux.createStore;
 
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICE_CREAM = "BUY_ICE_CREAM";
 
-//action creator function
+//buy ice cream action creator function
 function buyCake() {
     return {
         type: BUY_CAKE,
         info: "First redux action",
     };
 }
-
+//buy ice cream action creator function
+const buyIceCream = () => {
+    return {
+        type: BUY_ICE_CREAM,
+    };
+};
 //initial state
 const initialState = {
     numOfCakes: 10,
+    numOfIceCreams: 20,
 };
+
+const initialCakeState = {
+    numOfCakes: 10,
+};
+const initialIceCreamState = {
+    numOfIceCreams: 20,
+};
+
+//combined reducer
+
 //reducer function
-const reducer = (state = initialState, action) => {
+// const reducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case BUY_CAKE:
+//             return {
+//                 ...state,
+//                 numOfCakes: state.numOfCakes - 1,
+//             };
+//         case BUY_ICE_CREAM:
+//             return {
+//                 ...state,
+//                 numOfIceCreams: state.numOfIceCreams - 1,
+//             };
+//         default:
+//             return state;
+//     }
+// };
+
+// separate reducer for cake
+const cakeReducer = (state = initialCakeState, action) => {
     switch (action.type) {
         case BUY_CAKE:
             return {
                 ...state,
                 numOfCakes: state.numOfCakes - 1,
+            };
+        default:
+            return state;
+    }
+};
+
+//separate reducer for ice cream
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+    switch (action.type) {
+        case BUY_ICE_CREAM:
+            return {
+                ...state,
+                numOfIceCreams: state.numOfIceCreams - 1,
             };
         default:
             return state;
@@ -40,6 +88,6 @@ store.dispatch(buyCake());
 //we can also pass actions directly like this
 store.dispatch({ type: BUY_CAKE });
 store.dispatch(buyCake());
-store.dispatch(buyCake());
-store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 unsubscribe();
